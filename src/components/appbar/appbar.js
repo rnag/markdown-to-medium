55a074bc298a1e514d0a1eed6ad231385bfec3d3
Ritvik Nag <nagrim7@gmail.com>
2024-02-11 10:44:03 -0500

    add code link

--- src/components/appbar/appbar.js
+++ src/components/appbar/appbar.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-import {AppBar, Typography, Toolbar, Box} from '@mui/material';
+import {AppBar, Typography, Toolbar, IconButton} from '@mui/material';
 import { styled } from '@mui/material/styles';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import pkg from '../../../package.json';
 
+import './appbar.css'
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
 
     alignItems: 'flex-start',
@@ -16,32 +19,43 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
     },
 }));
 
-const Bar = props => {
+const Bar = _props => {
 
     return (
       <div>
-      <AppBar
-          position="relative"
-          sx={{ zIndex: 1 }}
-          elevation={4}
-      >
-          <StyledToolbar>
-
-              <Typography
-
-              variant="h5"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1, alignSelf: 'center' }}
+          <AppBar
+              position="relative"
+              sx={{zIndex: 1}}
+              elevation={4}
           >
-              Markdown to Medium
-          </Typography>
-          </StyledToolbar>
-      </AppBar>
+                  <StyledToolbar>
+
+                      <Typography
+
+                          variant="h5"
+                          noWrap
+                          component="div"
+                          sx={{flexGrow: 1, alignSelf: 'center'}}
+                      >
+                          Markdown to Medium
+                      </Typography>
+                      <IconButton
+                          aria-label="account of current user"
+                          aria-controls="code-appbar"
+                          aria-haspopup="true"
+                          color="inherit"
+                      >
+                          <a href={pkg.repository.url} rel="noopener noreferrer" target="_blank"
+                             className="link">
+                              <GitHubIcon sx={{ fontSize: '35px'}}/>
+                          </a>
+                      </IconButton>
+                  </StyledToolbar>
+          </AppBar>
       </div>
-  );
+);
 };
 
 export default connect(state => ({
-  menuactive: state.menuactive
+    menuactive: state.menuactive
 }))(Bar);
