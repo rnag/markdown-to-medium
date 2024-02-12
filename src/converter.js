4a3d6a0046e89090f94365a3c9da498ea1c0d5ed
Ritvik Nag <nagrim7@gmail.com>
2024-02-11 22:31:09 -0500

    feat: sanitize and prepare HTML  content for Medium

--- src/converter.js
+++ src/converter.js
@@ -35,7 +35,47 @@ function converter(content, allowUnsafe = false) {
         content = content.replace(/^\{[ \t]*%.+%[ \t]*}$/gm, '');
     }
 
-    return marked(content);
+    return cleanedContentForMedium(marked(content));
+}
+
+function cleanedContentForMedium(htmlContent) {
+    return (
+        htmlContent
+            // This is mainly to address code blocks that
+            // have two consecutive blank lines.
+            //
+            // Otherwise, Medium seems to split code blocks
+            // at this terminator.
+            .replace(/(\r\n|\r|\n){2}/g, '\n \n')
+            // This is to address bulleted list items in
+            // markdown that have a blank line between them.
+            //
+            // Medium seems to render them with a blank bulleted
+            // list item in between, otherwise.
+            .replace(
+                /<li>[\r\n]*<p>(.*)<\/p>[\r\n]*<\/li>/g,
+                '<li>$1</li>'
+            )
+            // This will replace embed links to other sites (I think
+            // Medium calls them mixtape embeds) so that they will be
+            // properly formatted in the post preview.
+            //
+            // The only thing needed is a manual `return` key press when
+            // cursor is at the end of the text.
+            .replace(
+                /<blockquote>[\r\n]*<p><a href="(.*)"><strong>.*<\/strong><\/a><\/p>[\r\n]*<\/blockquote>/g,
+                '<p><a href="$1">$1</a></p>'
+            )
+            // This will get rid of extraneous blank lines at the start and end
+            // of blockquotes that show up in the Medium editor otherwise.
+            .replace(
+                /<blockquote>[\r\n]*<p>(.*)<\/p>[\r\n]*<\/blockquote>/g,
+                '<blockquote><em>$1</em></blockquote>'
+            )
+            // This will replace markdown-flavor text inside underscore
+            // like `_{ anything }_` with `<em>{ anything }</em>`
+            .replace(/_([^\n]+)_/g, '<em>$1</em>')
+    );
 }
 
 export default converter;
